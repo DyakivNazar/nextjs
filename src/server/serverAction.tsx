cf4206d386922ed2213cd2d1a4b4834fb02f8f47
DyakivNazar <nazar2005.03.21@gmail.com>
2025-10-24 01:12:45 +0300

    homework 3

--- src/server/serverAction.tsx
+++ src/server/serverAction.tsx
@@ -4,6 +4,6 @@ import { ICar } from '@/models/ICar';
 import { revalidatePath } from 'next/cache';
 
 export const addCarByServer = async (car: ICar) => {
-    await addCar(car); // додає авто через API
-    revalidatePath('/'); // оновлює головну сторінку після сабміту
+    await addCar(car);
+    revalidatePath('/');
 };
